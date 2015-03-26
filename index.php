728899c0decf468fde34f8dabde4006b7228b24f
TTM-GA1415 <fulhuve@hotmail.com>
2015-03-26 11:12:09 +0100

    trissaker 2603

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Trisslotten!</title>
+        <link rel="stylesheet" href="reset.css">
+        <link rel="stylesheet" href="trisslott.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div class="trisslogo">
+                <img src="bilder/triss.jpg">
+            </div>
+            <div class="grupper">
+                <div class="gruppcell nr1">
+                    <h2>Bank</h2>
+                </div>
+                <div class="gruppcell nr2"></div>
+                <div class="gruppcell nr3">
+                    <h2>Statistik</h2>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
