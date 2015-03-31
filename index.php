017a00a95b2670ff5480a9323322f35452e08855
Benn024 <martin.benno.klevbrand@gmail.com>
2015-03-31 11:49:28 +0200

    popup jquery, klar med design

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
         <title>Trisslotten!</title>
         <link rel="stylesheet" href="reset.css">
         <link rel="stylesheet" href="trisslott.css">
+
+        <?-- font --?>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,700' rel='stylesheet' type='text/css'>
+        
+        <?-- jquery --?>
+        <script src="jquery-1.11.2.min.js"></script>
     </head>
     <body>
         <div id="wrapper">
@@ -15,11 +21,48 @@
                 <div class="gruppcell nr1">
                     <h2>Bank</h2>
                 </div>
-                <div class="gruppcell nr2"></div>
+                <div class="gruppcell nr2">
+                    <div class="bild_header">
+                        <img src="http://placehold.it/220x80">
+
+                    </div>
+                    <div class="skrapruta">
+
+                    </div>
+                    <div class="extrachans cirkelBas">
+
+                    </div>
+                    <div class="kontrollruta">
+
+                    </div>
+                    <div class="instruktioner">
+                        <h4>Instruktioner</h4>
+                        <p>Klicka för att lära om reglerna</p>
+                    </div>
+                </div>
                 <div class="gruppcell nr3">
                     <h2>Statistik</h2>
                 </div>
             </div>
         </div>
+        
+        <script>
+            $(function() {
+                $('#contact').on('click', function() {
+                    if ($(this).hasClass('selected')) {
+                        deselect($(this));
+                    } else {
+                        $(this).addClass('selected');
+                        $('.pop').slideFadeToggle();
+                    }
+                    return false;
+                });
+
+                $('.close').on('click', function() {
+                    deselect($('#contact'));
+                    return false;
+                });
+            });
+        </script>
     </body>
 </html>
